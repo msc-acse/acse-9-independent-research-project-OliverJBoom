04db0c0b58d1010b1861ed2887f891d43e966e28
oliver boom <ob14153@my.bristol.ac.uk>
2019-07-26 19:17:45 +0300

    Have to use Pytorch because Keras lacks determinism. Inititial deeplearning class created. In process of sketching out LSTM model

---
+++ deeplearning.py
@@ -0,0 +1,19 @@
+import numpy as np
+import random
+import torch
+
+
+def set_seed(seed):
+    """
+    Use this to set ALL the random seeds to a fixed value and take out any randomness from cuda kernels
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False  ##uses the inbuilt cudnn auto-tuner to find the fastest convolution algorithms. -
+    torch.backends.cudnn.enabled   = False
+
+    return True
