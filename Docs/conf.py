924d2ac3ab204653a2294b74d6d596d2ac33e8ae
OliverJBoom <ob14153@my.bristol.ac.uk>
2019-08-09 11:43:50 +0100

    Added PDF documentaPDF documentation.

--- Docs/conf.py
+++ Docs/conf.py
@@ -161,27 +161,4 @@ texinfo_documents = [
 ]
 
 
-# -- Options for Epub output -------------------------------------------------
 
-# Bibliographic Dublin Core info.
-epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
-
-
-# -- Extension configuration -------------------------------------------------
-
-# -- Options for todo extension ----------------------------------------------
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
