aa2055b2abc3adb74c4518df09d5ee0b5d17f743
OliverJBoom <ob14153@my.bristol.ac.uk>
2019-08-13 14:46:25 +0100

    RTD theme added

--- Docs/conf.py
+++ Docs/conf.py
@@ -44,8 +44,11 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
 ]
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
