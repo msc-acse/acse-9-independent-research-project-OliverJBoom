2ca0521d473bee914d5a79e79759e125780306d3
OliverJBoom <ob3618@ic.ac.uk>
2019-08-23 12:26:42 +0100

    Added requirements to setuptools

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
 import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 setuptools.setup(
-     name='dokr',  
-     version='0.1',
-     scripts=['dokr'] ,
-     author="Deepak Kumar",
-     author_email="deepak.kumar.iet@gmail.com",
-     description="A Docker and AWS utility package",
+     name='Foresight',
+     version='1.0.1',
+     author="Oliver Boom",
+     author_email="ob3618@ic.ac.uk",
+     license='MIT',
+     description="A package for price forecasting using LSTM networks",
      long_description=long_description,
    long_description_content_type="text/markdown",
-     url="https://github.com/javatechy/dokr",
+     url="https://github.com/msc-acse/acse-9-independent-research-project-OliverJBoom",
+     download_url = "https://github.com/msc-acse/acse-9-independent-research-project-OliverJBoom/archive/1.0.1.tar.gz",
      packages=setuptools.find_packages(),
+     install_requires=[
+                "numpy">=1.16.2,
+                "pandas">=0.24.2,
+                "pmdarima">=1.2.1,
+                "matplotlib">=3.0.3,
+                "psycopg2">=2.7.6.1,
+                "pytest",
+                "scikit-learn">=0.20.3,
+                "statsmodels">=0.9.0,
+                "torch">=1.1.0,
+      ],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
