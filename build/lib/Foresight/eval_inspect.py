d1a84dcb18ae88641ae24e7149c1d3b8d34a57bf
OliverJBoom <ob3618@ic.ac.uk>
2019-08-23 11:38:41 +0100

    Creating PIp

---
+++ build/lib/Foresight/eval_inspect.py
@@ -0,0 +1,190 @@
+# Author: Oliver Boom
+# Github Alias: OliverJBoom
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+
+"""This module include a set of functions that are used to evaluate and
+inspect the time series in the dataset."""
+
+
+def check_length(universe_dict):
+    """Checks the name of all the DataFrames in the dictionary of time series.
+
+    :param universe_dict:       The dictionary of time series
+    :type  universe_dict:       dict
+    """
+    for df_name in universe_dict:
+        print(len(universe_dict[df_name]))
+
+
+def visualise_df(df):
+    """Visualises each time series in a DataFrame.
+
+    :param df:                  The DataFrame of time series to visualise
+    :type  df:                  pd.DataFrame
+    """
+    fig, ax_arr = plt.subplots(int(len(df.columns) / 2), 2,
+                               figsize=(4 * 10, 4 * len(df.columns)))
+
+    for ax, df_name in zip(ax_arr.flatten(), df.columns):
+        ax.set_title(df_name)
+        ax.plot(df.index, df[df_name])
+        ax.grid()
+        ax.legend()
+
+    plt.show()
+
+
+def check_day_frequency(df, col_name='ds'):
+    """Creates a bar chart showing the frequency of the days of the week.
+
+    Used to check that only business days are included in the dataset, and
+    that there is a roughly equal distribution of entries across the week.
+
+    :param df:               A DataFrame containing the time series to check
+    :type  df:               pd.DataFrame
+
+    :param col_name:     The name of the column of interest
+    :type  col_name:     string
+    """
+    df["day"] = df[col_name].apply(lambda x: x.weekday_name)
+    print(df['day'].value_counts())
+    df['day'].value_counts().plot(kind='bar')
+
+
+def df_std(df, col_name):
+    """Calculates standard deviation of a DataFrames column.
+
+    :param df:                    A DataFrame of time series
+    :type  df:                    pd.DataFrame
+
+    :param col_name:              The column of interest
+    :type  col_name:              string
+
+    :return:                      The standard deviation of the series
+    :rtype:                       float
+    """
+    return df[[col_name]].stack().std()
+
+
+def inverse_log_returns(original_prices, log_returns, lag=5, offset=0):
+    """Takes a DataFrame of predicted log returns and original
+    prices and returns an array of predicted absolute prices
+
+    The offset parameter moves the series forwards or backwards to
+    align the series with the DataFrame it might be appended to.
+
+    :param original_prices:  A DataFrame of absolute prices
+    :type  original_prices:  pd.DataFrame
+
+    :param log_returns:      A DataFrame of log returns
+    :type  log_returns:      pd.DataFrame
+
+    :param lag:              The lag in days between series
+    :type  lag:              int
+
+    :param offset:          Amount to offset the series forwards of backwards
+    :type  offset:          int
+
+    :return:                The raw prices given by the log returns
+    :rtype:                 pd.Series
+    """
+    assert isinstance(log_returns, pd.DataFrame)
+    assert isinstance(original_prices, pd.DataFrame)
+    if offset == 0:
+        return (original_prices.shift(offset).values[:-lag] *
+                np.exp(log_returns[:-lag])).values.ravel()
+    else:
+        return (original_prices.shift(offset).values *
+                np.exp(log_returns)).values.ravel()
+
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    """Calculates the mean absolute percentage error between two arrays.
+
+    :param y_true:            The observed values
+    :type  y_true:            np.array
+
+    :param y_pred:            The predicted values
+    :type  y_pred:            np.array
+
+    :return:                  The mean absolute percentage error of the series
+    :rtype:                   float
+    """
+    return 100 * np.mean(np.abs(y_true - y_pred) / y_true)
+
+
+def evaluate(y_true, y_pred, log_ret=False):
+    """Calculates the error metrics for between two arrays.
+
+    The error metrics calculated are:
+        Means Squared Error
+        Mean Absolute Error
+        Mean Directional Accuracy
+
+    For a log returns series the definition of mean directional accuracy
+    changes. This is as for a log return series it is the signum values of the
+    series that details which direction the series has moved. This is as a log
+    return series is the first difference of the original series. For raw price
+    The signal needs to be differenced before the signum function is applied.
+
+
+    :param y_true:            The observed values
+    :type  y_true:            np.array
+
+    :param y_pred:            The predicted values
+    :type  y_pred:            np.array
+
+    :param log_ret:           Whether the series compared are log returns
+    :type  log_ret:           bool
+
+    :return error_metrics:    The error metrics of the series
+    :rtype:                   List
+    """
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+
+    if log_ret:
+        mda = mean_directional_accuracy_log_ret(y_true, y_pred)
+    else:
+        mda = mean_directional_accuracy(y_true, y_pred)
+
+    error_metrics = [mse, mae, mda]
+    return error_metrics
+
+
+def mean_directional_accuracy_log_ret(y_true, y_pred):
+    """Calculates the mean directional accuracy error metric between
+    two series of log returns.
+
+    :param y_true:           The observed values
+    :type  y_true:           np.array
+
+    :param y_pred:           The predicted values
+    :type  y_pred:           np.array
+
+    :return:                 The mean directional accuracy of the series
+    :rtype:                  float
+    """
+    return np.mean(np.sign(y_true) == np.sign(y_pred))
+
+
+def mean_directional_accuracy(y_true, y_pred):
+    """Calculated the mean directional accuracy error metric
+    between two series.
+
+    :param y_true:           The observed values
+    :type  y_true:           np.array
+
+    :param y_pred:           The predicted values
+    :type  y_pred:           np.array
+
+    :return:                 The mean directional accuracy of the series
+    :rtype:                  float
+    """
+    return np.mean(np.sign(y_true[1:, :] - y_true[:-1, :]) == np.sign(y_pred[1:, :] - y_pred[:-1, :]))
---
+++ build/lib/Foresight/models.py
@@ -0,0 +1,390 @@
+# Author: Oliver Boom
+# Github Alias: OliverJBoom
+
+import torch
+import torch.nn as nn
+
+"""This module contains the architectures for long short term memory
+neural networks"""
+
+
+class LSTM(nn.Module):
+    """A Long Short Term Memory network model with an additional dense layer
+
+    :param num_features:      The number of features in the dataset
+    :type  num_features:      int
+
+    :param hidden_dim:        The number of neurons in the LSTMs hidden layer/s
+    :type  hidden_dim:        int
+
+    :param dense_hidden:      The number of neurons in the dense layers
+    :type  dense_hidden:      int
+
+    :param output_dim:        The number of neurons in the output layer
+    :type  output_dim:        int
+
+    :param batch_size:        The number of items in each batch
+    :type  batch_size:        int
+
+    :param series_length:     The length of the time series
+    :type  series_length:     Int
+
+    :param device:            The device to run on (Cpu or CUDA)
+    :type  device:            string
+
+    :param dropout:           The probability of dropout
+    :type  dropout:           float
+
+    :param num_layers:        The number of stacked LSTM layers
+    :type  num_layers:        int
+    """
+
+    def __init__(self, num_features, hidden_dim, dense_hidden, output_dim,
+                 batch_size, series_length, device,
+                 dropout=0.1, num_layers=2):
+        super(LSTM, self).__init__()
+
+        # Number of features
+        self.num_features = num_features
+
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+
+        # Number of neurons in the dense layer
+        self.dense_hidden = dense_hidden
+
+        # Number of hidden layers
+        self.num_layers = num_layers
+
+        # The output dimensions
+        self.output_dim = output_dim
+
+        # Batch Size
+        self.batch_size = batch_size
+
+        # Length of sequence
+        self.series_length = series_length
+
+        # CPU or GPU
+        self.device = device
+
+        self.dropout = dropout
+
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(
+            input_size=self.num_features,
+            hidden_size=self.hidden_dim,
+            dropout=self.dropout,
+            num_layers=self.num_layers)
+
+        # Defining the Dense Layers
+        self.fc = nn.Sequential(
+            nn.Linear(self.hidden_dim, self.dense_hidden),
+            nn.ReLU(),
+            nn.Dropout(self.dropout),
+            nn.Linear(self.dense_hidden, self.output_dim))
+
+    def init_hidden(self, batch_size):
+        """Initialised the hidden state to be zeros. This clears the hidden
+        state between batches. If you are running a stateful LSTM then this
+        needs to be changed.
+
+        To change to a stateful LSTM requires not detaching the backprop and
+        storing the computational graph. This strongly increases runtime and
+        shouldn't make a big difference. Hence a stateful LSTM was not used.
+
+        :param batch_size:          The batch size to be zeroed
+        :type  batch_size:          string
+        """
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).to(
+            self.device)
+
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).to(
+            self.device)
+        return c0, h0
+
+    def forward(self, x):
+        """Forward pass through the neural network
+
+        :param x:          The input into the network
+        :type  x:          torch.Tensor
+        """
+        # Adjust to a variable batch size
+        batch_size = x.size()[0]
+        series_length = x.size()[1]
+
+        # Making sure the series and batch haven't been mis-permuted
+        assert (series_length == self.series_length)
+
+        # Keeps the dimensions constant regardless of batch size
+        x = x.contiguous().view(series_length, batch_size, -1)
+
+        # Initialises the hidden states
+        h0, c0 = self.init_hidden(batch_size)
+
+        # Pass through through LSTM layer
+        # Only the x is of interest
+        x, (hn, cn) = self.lstm(x, (h0, c0))
+
+        # Output is seq to seq but only want seq to val
+        # So only use the final slice of the LSTM outputted sequence
+        x = x[-1]
+
+        # Fully connected dense layers with dropout
+        x = self.fc(x)
+
+        return x
+
+
+class LSTM_shallow(nn.Module):
+    """A Long Short Term Memory network model that passes staight
+    from the LSTM layer to predictions
+
+    :param num_features:      The number of features in the dataset
+    :type  num_features:      int
+
+    :param hidden_dim:        The number of neurons in the LSTMs hidden layer/s
+    :type  hidden_dim:        int
+
+    :param output_dim:        The number of neurons in the output layer
+    :type  output_dim:        int
+
+    :param batch_size:        The number of items in each batch
+    :type  batch_size:        int
+
+    :param series_length:     The length of the time series
+    :type  series_length:     Int
+
+    :param device:            The device to run on (Cpu or CUDA)
+    :type  device:            string
+
+    :param dropout:           The probability of dropout
+    :type  dropout:           float
+
+    :param num_layers:        The number of stacked LSTM layers
+    :type  num_layers:        int
+    """
+    def __init__(self, num_features, hidden_dim, output_dim,
+                 batch_size, series_length, device,
+                 dropout=0.1, num_layers=2):
+        super(LSTM_shallow, self).__init__()
+
+        # Number of features
+        self.num_features = num_features
+
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+
+        # Number of hidden layers
+        self.num_layers = num_layers
+
+        # The output dimensions
+        self.output_dim = output_dim
+
+        # Batch Size
+        self.batch_size = batch_size
+
+        # Length of sequence
+        self.series_length = series_length
+
+        # CPU or GPU
+        self.device = device
+
+        self.dropout = dropout
+
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(
+            input_size=self.num_features,
+            hidden_size=self.hidden_dim,
+            dropout=self.dropout,
+            num_layers=self.num_layers)
+
+        # Defining the Dense Layers
+        self.fc = nn.Linear(self.hidden_dim, self.output_dim)
+
+    def init_hidden(self, batch_size):
+        """Initialised the hidden state to be zeros. This clears the hidden
+        state between batches. If you are running a stateful LSTM then this
+        needs to be changed.
+
+        To change to a stateful LSTM requires not detaching the backprop and
+        storing the computational graph. This strongly increases runtime and
+        shouldn't make a big difference. Hence a stateful LSTM was not used.
+
+        :param batch_size:          The batch size to be zeroed
+        :type  batch_size:          string
+        """
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).to(
+            self.device)
+
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).to(
+            self.device)
+        return c0, h0
+
+    def forward(self, x):
+        """Forward pass through the neural network
+
+        :param x:          The input into the network
+        :type  x:          torch.Tensor
+        """
+        # Adjust to a variable batch size
+        batch_size = x.size()[0]
+        series_length = x.size()[1]
+
+        # Making sure the series and batch haven't been mis-permuted
+        assert (series_length == self.series_length)
+
+        # Keeps the dimensions constant regardless of batch size
+        x = x.contiguous().view(series_length, batch_size, -1)
+
+        # Initialises the hidden states
+        h0, c0 = self.init_hidden(batch_size)
+
+        # Pass through through LSTM layer
+        # Only the x is of interest
+        x, (hn, cn) = self.lstm(x, (h0, c0))
+
+        # Output is seq to seq but only want seq to val
+        # So only use the final slice of the LSTM outputted sequence
+        x = x[-1]
+
+        # Passes staight from LSMT through to prediction layer
+        x = self.fc(x)
+
+        return x
+
+
+class LSTM_deeper(nn.Module):
+    """A Long Short Term Memory network model with two additional dense layers
+
+    :param num_features:      The number of features in the dataset
+    :type  num_features:      int
+
+    :param hidden_dim:        The number of neurons in the LSTMs hidden layer/s
+    :type  hidden_dim:        int
+
+    :param dense_hidden:      The number of neurons in the first dense layer
+    :type  dense_hidden:      int
+
+    :param dense_hidden_2:    The number of neurons in the second dense layer
+    :type  dense_hidden_2:    int
+
+    :param output_dim:        The number of neurons in the output layer
+    :type  output_dim:        int
+
+    :param batch_size:        The number of items in each batch
+    :type  batch_size:        int
+
+    :param series_length:     The length of the time series
+    :type  series_length:     Int
+
+    :param device:            The device to run on (Cpu or CUDA)
+    :type  device:            string
+
+    :param dropout:           The probability of dropout
+    :type  dropout:           float
+
+    :param num_layers:        The number of stacked LSTM layers
+    :type  num_layers:        int
+    """
+    def __init__(self, num_features, hidden_dim, dense_hidden, dense_hidden_2, output_dim,
+                 batch_size, series_length, device,
+                 dropout=0.1, num_layers=2):
+        super(LSTM_deeper, self).__init__()
+
+        # Number of features
+        self.num_features = num_features
+
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+
+        # Number of neurons in the dense layer
+        self.dense_hidden = dense_hidden
+
+        # Number of neurons in the dense layer
+        self.dense_hidden_2 = dense_hidden_2
+
+        # Number of hidden layers
+        self.num_layers = num_layers
+
+        # The output dimensions
+        self.output_dim = output_dim
+
+        # Batch Size
+        self.batch_size = batch_size
+
+        # Length of sequence
+        self.series_length = series_length
+
+        # CPU or GPU
+        self.device = device
+
+        self.dropout = dropout
+
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(
+            input_size=self.num_features,
+            hidden_size=self.hidden_dim,
+            dropout=self.dropout,
+            num_layers=self.num_layers)
+
+        # Defining the Dense Layers
+        self.fc = nn.Sequential(
+            nn.Linear(self.hidden_dim, self.dense_hidden),
+            nn.ReLU(),
+            nn.Dropout(self.dropout),
+            nn.Linear(self.dense_hidden, self.dense_hidden_2),
+            nn.ReLU(),
+            nn.Dropout(self.dropout),
+            nn.Linear(self.dense_hidden_2, self.output_dim),)
+
+    def init_hidden(self, batch_size):
+        """Initialised the hidden state to be zeros. This clears the hidden
+        state between batches. If you are running a stateful LSTM then this
+        needs to be changed.
+
+        To change to a stateful LSTM requires not detaching the backprop and
+        storing the computational graph. This strongly increases runtime and
+        shouldn't make a big difference. Hence a stateful LSTM was not used.
+
+        :param batch_size:          The batch size to be zeroed
+        :type  batch_size:          string
+        """
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).to(
+            self.device)
+
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).to(
+            self.device)
+        return c0, h0
+
+    def forward(self, x):
+        """Forward pass through the neural network
+
+        :param x:          The input into the network
+        :type  x:          torch.Tensor
+        """
+        # Adjust to a variable batch size
+        batch_size = x.size()[0]
+        series_length = x.size()[1]
+
+        # Making sure the series and batch haven't been mis-permuted
+        assert (series_length == self.series_length)
+
+        # Keeps the dimensions constant regardless of batch size
+        x = x.contiguous().view(series_length, batch_size, -1)
+
+        # Initialises the hidden states
+        h0, c0 = self.init_hidden(batch_size)
+
+        # Pass through through LSTM layer
+        # Only the x is of interest
+        x, (hn, cn) = self.lstm(x, (h0, c0))
+
+        # Output is seq to seq but only want seq to val
+        # So only use the final slice of the LSTM outputted sequence
+        x = x[-1]
+
+        # Fully connected dense layers with dropout
+        x = self.fc(x)
+
+        return x
