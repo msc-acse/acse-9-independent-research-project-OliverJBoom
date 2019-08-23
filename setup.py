85f3320c70c1462ea9a0e5f502ff1f49b8cfabe2
OliverJBoom <ob3618@ic.ac.uk>
2019-08-23 13:43:31 +0100

    Removed autoinstall

--- setup.py
+++ setup.py
@@ -14,17 +14,6 @@ setuptools.setup(
      url="https://github.com/msc-acse/acse-9-independent-research-project-OliverJBoom",
      download_url = "https://github.com/msc-acse/acse-9-independent-research-project-OliverJBoom/archive/1.0.0.tar.gz",
      packages=setuptools.find_packages(),
-     install_requires=[
-                "numpy>=1.16.2",
-                "pandas>=0.24.2",
-                "pmdarima>=1.2.1",
-                "matplotlib>=3.0.3",
-                "psycopg2>=2.7.6.1",
-                "pytest",
-                "scikit-learn>=0.20.3",
-                "statsmodels>=0.9.0",
-                "torch>=1.1.0",
-      ],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
